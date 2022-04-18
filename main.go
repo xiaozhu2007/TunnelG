84d4c18072f0f585d8dbc391ed91aba72f7b8d06
甜力怕 <63779340+xiaozhu2007@users.noreply.github.com>
2022-04-18 14:38:41 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+	"sync"
+  "flag"
+)
+
+var (
+    ports   = flag.Int("ports", 3000, "Ports to forward")
+    backend = flag.Int("backend", 3001, "Backend to receive requests")
+)
+
+func main() {
+	flag.Parse()
+	var wg sync.WaitGroup
+	ps := strings.Split(*ports, ",")
+	for _, p := range ps {
+		wg.Add(1)
+		go func(p string) {
+			defer wg.Done()
+
+			l, err := net.Listen("tcp", ":"+p)
+			if err != nil {
+				log.Fatalf("Failed to setup listener: %v", err)
+			}
+			defer l.Close()
+
+			fmt.Printf("0.0.0.0:%s -> %s:%s\n", p, *backend, p)
+
+			for {
+				conn, err := l.Accept()
+				if err != nil {
+					log.Fatalf("ERROR: failed to accept listener: %v", err)
+				}
+				go forward(conn, *backend+":"+p)
+			}
+		}(p)
+	}
+	wg.Wait()
+}
+
+func forward(conn net.Conn, b string) {
+	client, err := net.Dial("tcp", b)
+	if err != nil {
+		log.Fatalf("Dial failed: %v", err)
+	}
+	go func() {
+		defer client.Close()
+		defer conn.Close()
+		io.Copy(client, conn)
+	}()
+	go func() {
+		defer client.Close()
+		defer conn.Close()
+		io.Copy(conn, client)
+	}()
+}
